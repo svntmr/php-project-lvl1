deb18428dcc920844eae6e1409e58f4ccd4bdc24
svn.tmr <svn.tmr@bk.ru>
2020-03-17 18:55:59 +0300

    fix mentor issue #8
    move ll code in one main function

--- src/engine.php
+++ src/engine.php
@@ -8,7 +8,12 @@ const ROUNDS = 3;
 
 function runGame(string $gameDescription, callable $getGameData)
 {
-    $name = welcome($gameDescription);
+    line('Welcome to the Brain Games!');
+    line($gameDescription);
+    line();
+    $name = prompt('May I have your name? ');
+    line("Hello, %s!", $name);
+    line();
     for ($i = 0; $i < ROUNDS; $i++) {
         ['question' => $question, 'correctAnswer' => $correctAnswer] = $getGameData();
         line("Question: {$question}");
@@ -16,31 +21,10 @@ function runGame(string $gameDescription, callable $getGameData)
         if ($answer == $correctAnswer) {
             line('Correct!');
         } else {
-            bue($answer, $correctAnswer, $name);
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+            line("Let's try again, $name!");
+            die();
         }
     }
-    congratulationsTo($name);
-}
-
-function welcome(string $gameDescription)
-{
-    line('Welcome to the Brain Games!');
-    line($gameDescription);
-    line();
-    $name = prompt('May I have your name? ');
-    line("Hello, %s!", $name);
-    line();
-    return $name;
-}
-
-function bue(string $answer, string $rightAnswer, string $name)
-{
-    line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
-    line("Let's try again, {$name}!");
-    die();
-}
-
-function congratulationsTo(string $name)
-{
-    line("Congratulations, {$name}!");
+    line("Congratulations, $name!");
 }
