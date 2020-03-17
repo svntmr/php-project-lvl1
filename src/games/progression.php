41e4835706b39cb35a2e9c9ea357d6c0fe2e4308
svn.tmr <svn.tmr@bk.ru>
2020-03-17 18:11:57 +0300

    fix mentor issue #5
    change variable name

--- src/games/progression.php
+++ src/games/progression.php
@@ -9,8 +9,8 @@ function run()
     $gameDescription = 'What number is missing in the progression?';
     $getGameData = function () {
         $logic = [];
-        [$removedElement, $numbersArray] = createProgressionArray();
-        $logic['question'] = implode(' ', $numbersArray);
+        [$removedElement, $numbers] = createProgressionArray();
+        $logic['question'] = implode(' ', $numbers);
         $logic['correctAnswer'] = $removedElement;
         return $logic;
     };
@@ -22,10 +22,10 @@ function createProgressionArray()
     $start = rand(1, 50);
     $step = rand(1, 10);
     $steps = 10;
-    $numbersArray = range($start, $start + $step * $steps, $step);
-    $randomKey = array_rand($numbersArray);
-    $removedElement = $numbersArray[$randomKey];
-    $numbersArray[$randomKey] = '..';
+    $numbers = range($start, $start + $step * $steps, $step);
+    $randomKey = array_rand($numbers);
+    $removedElement = $numbers[$randomKey];
+    $numbers[$randomKey] = '..';
 
-    return [$removedElement, $numbersArray];
+    return [$removedElement, $numbers];
 }
