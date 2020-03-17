cebf941b7a71ab6bf85cb539f04b2652178f9f79
svn.tmr <svn.tmr@bk.ru>
2020-03-17 18:05:27 +0300

    fix mentor issue #3
    change excessive function to two random integers

--- src/games/divisor.php
+++ src/games/divisor.php
@@ -9,21 +9,13 @@ function run()
     $gameDescription = 'Find the greatest common divisor of given numbers.';
     $getGameData = function () {
         $logic = [];
-        $parameters = generateQuestion();
-        $question = implode(' ', $parameters);
-        $logic['question'] = $question;
-        [$a, $b] = $parameters;
-        $logic['correctAnswer'] = gcd($a, $b);
+        $firstNum = rand(1, 50);
+        $secondNum = rand(1, 50);
+        $logic['question'] = "{$firstNum} {$secondNum}";
+        $logic['correctAnswer'] = gcd($firstNum, $secondNum);
         return $logic;
     };
     runGame($gameDescription, $getGameData);
-}
-
-function generateQuestion()
-{
-    $firstNumber = rand(1, 50);
-    $secondNumber = rand(1, 50);
-    return [$firstNumber, $secondNumber];
 }
 
 function gcd($a, $b)
